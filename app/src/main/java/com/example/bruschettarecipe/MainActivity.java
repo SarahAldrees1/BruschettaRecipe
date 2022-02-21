64d7fce28dc2c24d82b9feed61b786f2245ec27c
SarahAldrees1 <aldreessarah@gail.com>
2022-02-21 12:22:59 +0300

    fff

---
+++ app/src/main/java/com/example/bruschettarecipe/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.bruschettarecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+//gergrtgt
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Recipe.class));
+            }
+        });
+    }
+}
